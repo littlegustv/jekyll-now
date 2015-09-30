7e74aa3c992ccc9e1c747abc56621d05914c3f16
littlegustv <echo29x@gmail.com>
2015-09-30 13:37:49 -0400

    styling change, mostly

--- apps/blocks/background.js
+++ apps/blocks/background.js
@@ -1,7 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     'outerBounds': {
-      'width': 640,
+      'width': 800,
       'height': 800
     }
   });
