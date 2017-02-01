99c91181315d26f656badae6dcba3511c21bb866
littlegustv <echo29x@gmail.com>
2017-01-31 21:08:57 -0500

    mobile improvements for 'ships'

--- games/ships/js/main.js
+++ games/ships/js/main.js
@@ -1,8 +1,27 @@
 var gameWorld;
 
+// add to raindrop
+function resizeCanvas(canvas) {
+  canvas.style.width = "", canvas.style.height = "";
+  var ratio = canvas.width / canvas.height;
+  // wider
+  if (window.innerWidth / window.innerHeight > ratio)
+  {
+    canvas.style.height = window.innerHeight + "px";
+    scale = window.innerHeight / canvas.height;
+  } else {
+    canvas.style.width = window.innerWidth + "px";
+    scale = window.innerWidth / canvas.width;
+  }
+}
+
 window.addEventListener("DOMContentLoaded", function () {
 	/* create a game world, including canvas, context and configuration... load resources */
 	gameWorld = Object.create(World).init();
+
+  // add to raindrop startup
+  resizeCanvas(gameWorld.canvas);
+  window.addEventListener('resize', function () { resizeCanvas(gameWorld.canvas); });
 
 	//var s = Object.create(Scene).init("mainmenu");
 	//gameWorld.scene = s;
