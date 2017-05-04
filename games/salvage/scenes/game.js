18065d9aa0f364189631294b38e179924e81193e
echo29x <echo29x@gmail.com>
2017-05-04 12:29:47 -0400

    pause/unpause behavior handling, enemy weapons framework (basiss), two-player object resolution

--- games/salvage/scenes/game.js
+++ games/salvage/scenes/game.js
@@ -22,46 +22,61 @@ var onStart = function () {
 	bg.add(Object.create(TiledBackground).init(gameWorld.width / 2, gameWorld.height - 16, gameWorld.width, 64, Resources.silhouette)).opacity = 0.3;
 	//planet.velocity = {x: 0, y: 0, angle: PI / 72};
 
-	this.player = fg.add(Object.create(Sprite).init(gameWorld.width / 2, gameWorld.height / 2,Resources.viper));
-  this.player.family = "player";
-  this.player.addBehavior(Velocity);
-  this.player.addBehavior(Accelerate);
+	// player is on foreground, can rotate when game is paused
+	this.player_top = fg.add(Object.create(Sprite).init(gameWorld.width / 2, gameWorld.height / 2,Resources.viper));
+  //this.player_top.family = "player";
+  //this.player.addBehavior(Velocity);
+  //this.player.addBehavior(Accelerate);
 	//this.player.addBehavior(Contrail);
-  this.player.health = MAXHEALTH;
+  //this.player.health = MAXHEALTH;
 
+  //this.player.velocity = {x: 0, y: 0};
+  //this.player.cooldown = 0;
+	//this.player.salvage = 0;
+  var p = this.player_top;
+	//this.player.shoot = Weapons.double;
+  var player_bot = bg.add(Object.create(Entity).init(p.x, p.y, p.w, p.h));
+  player_bot.color = "red";
+  player_bot.setCollision(Polygon);
+	player_bot.damage = player_bot.addBehavior(Damage, {layer: bg, timer: 0, invulnerable: 1});
+	player_bot.shoot = Weapons.double;
+	
+	player_bot.addBehavior(Accelerate);
+	player_bot.addBehavior(Velocity);
+  player_bot.velocity = {x: 0, y: 0};
+  player_bot.acceleration = {x: 0, y: 0};
+  player_bot.opacity = 0;
+	player_bot.health = MAXHEALTH;
+	player_bot.salvage = 0;
+  player_bot.family = "player";
+  player_bot.collision.onHandle = function (object, other) {
+		if (object.damage.timer > 0) return;
+    if (other.family == "enemy") {
+      object.health -= 1;
+			object.damage.timer = DAMAGE_COOLDOWN;
+    }
+		if (object.health <= 0) {
+			object.alive = false;
+			p.alive = false;
+      gameWorld.playSound(Resources.hit);
+			gameWorld.setScene(0, true);
+		}
+  }
+	player_bot.delay = player_bot.addBehavior(Delay, {duration: 1, remove: false, callback: function () {
+		this.entity.velocity = {x: 0, y: 0};
+		this.entity.acceleration = {x: 0, y: 0};
+		s.pause();
+	}});
+
+  //player_bot.addBehavior(Follow, {target: p, offset: {angle: 0, x: false, y: false, z: false}});
+	this.player_top.addBehavior(Follow, {target: player_bot, offset: {angle: false, x: 0, y: 0, z: 0}});
+	this.player_bot = player_bot;
+	
   this.claw = fg.add(Object.create(Sprite).init(gameWorld.width / 2, - gameWorld.height / 2, Resources.claw));
   this.arm = fg.add(Object.create(Entity).init(gameWorld.width / 2, - gameWorld.height, 4, gameWorld.height));
   this.arm.addBehavior(Follow, {target: this.claw, offset: {x: 0, y: -gameWorld.height / 2 - 6}});
 
-	this.store = Object.create(Store).init(this.ui, this.player);
-  this.player.velocity = {x: 0, y: 0};
-  this.player.cooldown = 0;
-	this.player.salvage = 0;
-  var p = this.player;
-	this.player.shoot = Weapons.double;
-  var player_dummy = bg.add(Object.create(Entity).init(p.x, p.y, p.w, p.h));
-  player_dummy.color = "red";
-  player_dummy.setCollision(Polygon);
-	player_dummy.damage = this.player.addBehavior(Damage, {layer: bg, timer: 0, invulnerable: 1});
-
-  player_dummy.velocity = {x: 0, y: 0};
-  player_dummy.acceleration = {x: 0, y: 0};
-  player_dummy.opacity = 0;
-  player_dummy.family = "player";
-  player_dummy.collision.onHandle = function (object, other) {
-    if (other.family == "enemy") {
-      object.health -= 1;
-			object.cooldown = DAMAGE_COOLDOWN;
-    }
-
-    if (p.health <= 0) {
-      p.alive = false;
-      gameWorld.playSound(Resources.hit);
-			gameWorld.setScene(0, true);
-    }
-  }
-  player_dummy.addBehavior(Follow, {target: p, offset: {angle: 0, x: 0, y: 0, z: 0}});
-	
+	this.store = Object.create(Store).init(this.ui, this.player_bot);
   for (var i = 0; i < gameWorld.width / 4; i++) {
     bg.add(Object.create(Sprite).init(i * 4 + 2, 2, Resources.barrier));
   }  
@@ -81,9 +96,10 @@ var onStart = function () {
 	
   this.keydown = false;
   this.pause = function () {
-    this.bg.paused = 10000;
-    this.player.velocity = {x: 0, y: 0};
-    this.player.animation = 0;
+    this.bg.paused = true;
+    this.player_bot.velocity = {x: 0, y: 0};
+    this.player_bot.acceleration = {x: 0, y: 0};
+    this.player_bot.animation = 0;
   }
   this.pause();
   
@@ -109,20 +125,14 @@ var onStart = function () {
       }
       return;
     }
-		if (!s.player.locked) {
-			if (s.player.velocity.x === 0 && s.player.velocity.y === 0) {
-				s.player.angle = angle(s.player.x, s.player.y, e.x, e.y);
-			}
+		if (!s.player_top.locked) {
+			//if (s.player_bot.velocity.x === 0 && s.player_bot.velocity.y === 0) {
+				s.player_top.angle = angle(s.player_top.x, s.player_top.y, e.x, e.y);
+			//}
 		}
   }
   this.onMouseUp = function (e) {
 		return;
-    if (!s.player.locked) {
-			s.player.velocity = {x: 0, y: 0};
-			if (s.player.cooldown <= 0) {
-				s.pause();
-			}
-		}
   }
   this.onMouseDown = function (e) {
 		if (s.ui.active) {
@@ -131,46 +141,45 @@ var onStart = function () {
 				b.trigger();
 			}
 			return;
-		} else if (!s.player.locked) {
-      s.bg.paused = 0;
-      s.fg.paused = 0;
-      s.player.animation = 1;
-      //s.player.cooldown = 1;
-      s.player.velocity = {
-        x: Math.cos( s.player.angle) * 100,
-        y: Math.sin( s.player.angle) * 100
+		} else if (!s.player_bot.locked && s.bg.paused != 0) {
+      s.bg.paused = false;
+      s.fg.paused = false;
+			s.player_bot.angle = s.player_top.angle;
+      s.player_bot.animation = 1;
+      s.player_bot.velocity = {
+        x: Math.cos( s.player_bot.angle) * 100,
+        y: Math.sin( s.player_bot.angle) * 100
       }
-      s.player.acceleration = {
-        x: -s.player.velocity.x,
-        y: -s.player.velocity.y
+      s.player_bot.acceleration = {
+        x: -s.player_bot.velocity.x,
+        y: -s.player_bot.velocity.y
       }
       // create contrail sprite
       gameWorld.playSound(Resources.move);  
-      var d = s.player.layer.add(Object.create(Sprite).init(s.player.x, s.player.y, Resources.dust));
+      var d = s.player_bot.layer.add(Object.create(Sprite).init(s.player_bot.x, s.player_bot.y, Resources.dust));
       d.addBehavior(Velocity);
-      d.velocity = {x: -s.player.velocity.x / 2, y: -s.player.velocity.y / 2};
+      d.velocity = {x: -s.player_bot.velocity.x / 2, y: -s.player_bot.velocity.y / 2};
       d.addBehavior(FadeOut, {duration: 0.8});
-
-      s.player.addBehavior(Delay, {duration: 1, callback: function () {
-        this.entity.velocity = {x: 0, y: 0};
-        this.entity.acceleration = {x: 0, y: 0};
-        s.pause();
-      }});
+			s.player_bot.delay.set();
 		}
   }
   this.onKeyPress = function (e) {
-		if (!s.player.locked) {
+		if (!s.player_bot.locked) {
 			if (e.keyCode == 122) {
-				if (s.player.shoot(s.bg)) {
-  				var flash = s.bg.add(Object.create(Sprite).init(s.player.x + 9 * Math.cos(s.player.angle),
-            s.player.y + 9 * Math.sin(s.player.angle), Resources.flash));
+				if (s.bg.paused <= 0) return;
+				else {
+					s.bg.paused = false;      
+					s.fg.paused = false;
+					s.player_bot.angle = s.player_top.angle;
+					var delay = s.player_bot.shoot(s.bg)
+  				var flash = s.bg.add(Object.create(Sprite).init(s.player_bot.x + 9 * Math.cos(s.player_bot.angle),
+          s.player_bot.y + 9 * Math.sin(s.player_bot.angle), Resources.flash));
           flash.opacity = 0;
           flash.addBehavior(FadeIn, {duration: 0.1, maxOpacity: 1});
           flash.addBehavior(FadeOut, {maxOpacity: 1, duration: 0.1, delay: 0.2});
           flash.z = -1;
-          s.bg.paused = 0;      
-  				s.fg.paused = 0;
-        }
+					s.player_bot.delay.set(delay);
+				}
 			}			
 		}
   }
@@ -205,41 +214,34 @@ var onStart = function () {
 	]
 }
 var onUpdate = function (dt) {
-  // should convert this into behavior, eventually
-  if (this.bg.paused === 0) {
-    if (this.player.cooldown >= 0) {
-      this.player.cooldown -= dt;
-    } else if (this.player.cooldown > -1) {
-      if (this.player.velocity.x === 0 && this.player.velocity.y === 0) {
-        this.pause();  
-      }
-      this.player.cooldown = -1;
-      //this.player.cooldown = 0.3;
-    }      
-  }
+  
 	for (var i = 0; i < this.wave.length; i++) {
 		if (!this.wave[i].alive) this.wave.splice(i, 1);
 	}
-  if (this.bg.paused === 0 && this.wave.length <= 0) {
+  if (!this.bg.paused && this.wave.length <= 0) {
 		console.log('new wave');
 		this.current_wave += 1;
 		if (this.current_wave % 2 === 0) {
       var t = this;
-      t.bg.paused = 10000;
-			t.player.locked = true;
-			t.player.velocity = {x: 0, y: 0};
-      this.claw.lerpx = this.claw.addBehavior(Lerp, {object: this.claw, field: "x", goal: this.player.x, rate: 5});
-      this.claw.lerpy = this.claw.addBehavior(Lerp, {object: this.claw, field: "y", goal: this.player.y, rate: 5, callback: function () {
+      t.bg.paused = true;
+			t.player_bot.locked = true;
+			t.player_bot.velocity = {x: 0, y: 0};
+			t.player_bot.acceleration = {x: 0, y: 0};
+      this.claw.lerpx = this.claw.addBehavior(Lerp, {object: this.claw, field: "x", goal: this.player_bot.x, rate: 5});
+      this.claw.lerpy = this.claw.addBehavior(Lerp, {object: this.claw, field: "y", goal: this.player_bot.y, rate: 5, callback: function () {
         t.claw.animation = 1;
         t.claw.removeBehavior(t.claw.lerpx);
         t.claw.removeBehavior(t.claw.lerpy);
-        t.player.grabbed = t.player.addBehavior(Follow, {target: t.claw, offset: {x: 0, y: 0}});
+        t.player_bot.grabbed = t.player_bot.addBehavior(Follow, {target: t.claw, offset: {x: 0, y: 0}});
+        t.player_top.grabbed = t.player_top.addBehavior(Follow, {target: t.claw, offset: {x: 0, y: 0}});
         t.claw.lerpx = t.claw.addBehavior(Lerp, {object: t.claw, field: "x", goal: 3 * gameWorld.width / 4, rate: 5});
         t.claw.lerpy = t.claw.addBehavior(Lerp, {object: t.claw, field: "y", goal: gameWorld.height / 3, rate: 5, callback: function () {
     			t.store.open();
         	t.claw.removeBehavior(t.claw.lerpx);
         	t.claw.removeBehavior(t.claw.lerpy);
-          t.player.removeBehavior(t.player.grabbed);
+					// player jumps back to previous position here; but no matter, this whole store-open trigger is going to change, right? FIX ME!
+          t.player_bot.removeBehavior(t.player_bot.grabbed);
+          t.player_top.removeBehavior(t.player_top.grabbed);
         }});
       }})
       gameWorld.playSound(Resources.store);
@@ -247,7 +249,7 @@ var onUpdate = function (dt) {
 			var w = choose(this.waves);
 			gameWorld.playSound(Resources[choose(["spawn", "spawn2"])]);
 			for (var j = 0; j < w.length; j++) {
-				var enemy = spawn(this.bg, w[j], this.player);
+				var enemy = spawn(this.bg, w[j], this.player_bot);
 				this.wave.push(enemy);
 			}
 		}
