8bbc0d358abfa75e16422a4e8768a06510d47913
Benjamin Heller <echo29x@gmail.com>
2017-10-06 09:49:51 -0400

    mm

--- games/blocks/scenes/game.js
+++ games/blocks/scenes/game.js
@@ -17,26 +17,29 @@ var onStart = function () {
 			a.frame = 0;
 			game.playSound(Resources.explode);
 		}
+		b.eyes = bg.add(Object.create(Sprite).init(Resources.eyes));
+		b.eyes.add(Follow, {target: b, offset: {x: 0, y: 0, z: 1}});
 	}
-	var hammer = bg.add(Object.create(Sprite).init(Resources.hammer)).set({x: 8, y: 32, velocity: {x: 0, y: 0}, acceleration: {x: 0, y: 100}});
-	hammer.setCollision(Polygon);
-	hammer.collision.onHandle = function (a, b) {
+	var bomb = bg.add(Object.create(Sprite).init(Resources.bomb)).set({x: 8, y: 32, velocity: {x: 0, y: 0}, acceleration: {x: 0, y: 200}});
+	bomb.setCollision(Polygon);
+	bomb.collision.onHandle = function (a, b) {
 		if (a.nocollide) return;
-		a.velocity.y = -120;
+		a.velocity.y = -160;
 		a.nocollide = true;
 		a.add(Delay, {duration: 0.5, callback: function () { this.entity.nocollide = false; }});
 	}
-	hammer.add(Velocity);
-	hammer.add(Accelerate);
+	bomb.add(Velocity);
+	bomb.add(Accelerate);
+	bomb.add(Wrap, {min: {x: 0, y: 0}, max: {x: game.w, y: game.h}})
 
 	this.onKeyPress = function (e) {
 		console.log(e.keyCode);
 		switch (e.keyCode) {
-			case 100:
-				hammer.x += 16;
+			case 100, 39:
+				bomb.x += 16;
 				break;
-			case 97:
-				hammer.x -= 16;
+			case 97, 37:
+				bomb.x -= 16;
 				break;
 		}
 	}
