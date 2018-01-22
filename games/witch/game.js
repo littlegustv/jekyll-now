034a1daf65520bee6f894bb31ddbc8264bd565d2
Benny Heller <echo29x@gmail.com>
2018-01-21 20:23:28 -0500

    witch

--- games/witch/game.js
+++ games/witch/game.js
@@ -1,10 +1,195 @@
 var game = Object.create(World).init(320, 180, "index.json");
 
-//raindrop raindrop raindrop
-Entity.remove = function (obj) {
-	var i = this.behaviors.indexOf(obj);
-	obj.end();
-	this.behaviors.splice(i, 1);
-	return obj; // should this chain the behavior or the entity?
+var min = {x: 0, y: 20};
+var TILESIZE = 16;
+
+function at(a, b) {
+	return a.x === b.x && a.y === b.y;
+}
+
+function toGrid(p) {
+	return {x: Math.floor((p.x - min.x) / TILESIZE), y: Math.floor((p.y - min.y) / TILESIZE)};
+}
+function fromGrid(p) {
+	return {x: min.x + p.x * TILESIZE, y: min.y + p.y * TILESIZE};
+}
+
+var Fire = {
+	square: function (entity, points) {
+  	for (var i = 0; i < points.length; i++) {
+      var flame = entity.layer.add(Object.create(Sprite).init(Resources.fire)).set({x: points[i].x, y: points[i].y, color: "orange", w: TILESIZE, h: TILESIZE, z: entity.z - 1, family: 'fire'});
+      game.scene.summons.push(flame); // fix me: bad global(ish) object reliance!
+    }  	
+  }
+}
+
+// gridsize, offset
+var Spell = Object.create(Behavior);
+Spell.set = function (point) {
+	this.points.push(point);
 };
-Entity.removeBehavior = Entity.remove;
+Spell.shapes = [
+	// square
+  {
+  	name: 'square',
+    points: [
+      {d: 1, theta: 0},
+      {d: 1, theta: PI / 2},
+      {d: 1, theta: PI},
+      {d: 1, theta: 3 * PI / 2}
+    ]
+  },
+  // staircase
+  {
+    name: 'staircase',
+    points: [
+      {d: 1, theta: PI / 2},
+      {d: 1, theta: 0},
+      {d: 1, theta: PI / 2},
+      {d: 1, theta: 0}
+    ]
+  },
+  // u
+  {
+    name: 'u',
+    points: [
+      {d: 1, theta: PI / 2},
+      {d: 1, theta: 0},
+      {d: 1, theta: 3 * PI / 2}
+    ]
+  },
+  {
+  	name: 'triangle-down',
+    points: [
+    	{d: Math.sqrt(0.5), theta: PI / 4},
+      {d: Math.sqrt(0.5), theta: 7 * PI / 4},
+      {d: 1, theta: PI}
+    ]
+  }
+];
+// fix me: ALSO check reverse
+Spell.match = function (spell, attempt) {
+	var min = 1000;
+  for (var i = 0; i < spell.points.length; i++) {
+  	var m = spell.points.slice(i, spell.points.length).concat(spell.points.slice(0, i)).reduce(function (acc, cur, index) { return acc + Math.abs(attempt[index].theta - cur.theta) + Math.abs(attempt[index].d - cur.d); }, 0)
+    if (m < min) {
+    	min = m;
+    }
+  }
+  // try the spell in reverse
+  var reverse = spell.points.slice(0).reverse();
+  for (var i = 0; i < reverse.length; i++) {
+  	var m = reverse.slice(i, reverse.length).concat(reverse.slice(0, i)).reduce(function (acc, cur, index) { return acc + Math.abs(attempt[index].theta - ((PI + cur.theta) % PI2)) + Math.abs(attempt[index].d - cur.d); }, 0)
+    if (m < min) {
+    	min = m;
+    }
+  }
+  delete reverse;
+	return min;
+}
+// pattern match various shapes
+Spell.check = function () {
+	var points = [];
+  var t = this;
+  var max_distance = 0;
+  for (var i = 0; i < this.points.length; i++) {
+  	var p = this.points[i];
+    if (i + 1 < this.points.length) {
+      var q = this.points[(i + 1)];    
+    } else {
+    	var q = {x: this.entity.x, y: this.entity.y};    
+    }
+		var new_point = {d: distance(p.x, p.y, q.x, q.y), theta: modulo(angle(p.x, p.y, q.x, q.y), PI2)}
+  	if (new_point.d > max_distance) max_distance = new_point.d;
+    points.push(new_point);
+  }
+  // normalize distances
+	for (var i = 0; i < points.length; i++) {
+  	points[i].d = points[i].d / max_distance;
+  }  
+  
+  // start by assuming shape start is always the same (figure out rotations later)
+ 	var shapes = this.shapes.filter(function (shape) { return shape.points.length == points.length; });
+  if (shapes.length <= 0) console.log('no matching size');
+  else {
+  	shapes = shapes.sort(function (a, b) { 
+    	return t.match(a, points) - t.match(b, points);
+      	//b.points.reduce(function (acc, cur, index) { return acc + Math.abs(points[index].theta - cur.theta); }, 0);
+    });
+    var delta = t.match(shapes[0], points);
+    if (delta > this.threshold) {
+    	console.log('too different, sorry!', delta, shapes[0].name, shapes[0], points, this.points);
+    } else {
+      console.log('matched', shapes[0].name);
+      if (Fire[shapes[0].name]) {
+        Fire[shapes[0].name](this.entity, this.points);
+      }      
+    }    
+  }
+}
+Spell.reset = function () {
+	this.check();
+	this.points = [];
+}
+Spell.update = function (dt) {
+	if (this.time === undefined) this.time = 0;
+	//this.time += 10 * dt;
+	//if (this.time >= TILESIZE) this.time = 0;
+}
+/* https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm */
+Spell.draw = function (ctx) {
+  ctx.fillStyle = this.color;
+  ctx.globalAlpha = 0.9;
+  ctx.lineWidth = 2;
+	for (var i = 0; i < this.points.length; i++) {
+    var p0 = toGrid(this.points[i]);
+    if (i < this.points.length - 1) var p1 = toGrid(this.points[i + 1]);
+    else var p1 = toGrid(this.entity);
+    var dx = p1.x - p0.x;
+    var dy = p1.y - p0.y;
+    if (Math.abs(dx) > Math.abs(dy)) {
+      var derr = Math.abs(dy / dx);
+	    var y = p0.y;
+      var error = 0;
+      for (var x = p0.x; (sign(dx) == 1 ? x <= p1.x : x >= p1.x); (sign(dx) == 1 ? x++ : x--)) {
+        var pout = fromGrid({x: x, y: y});
+        ctx.fillRect(pout.x, pout.y, TILESIZE, TILESIZE);
+        ctx.drawImage(Resources.icon.image, pout.x, pout.y);
+        error += derr;
+        while (error >= 0.5) {
+          y = y + sign(dy) * 1;
+          error = error - 1;
+        }
+      }
+    } else {
+    	var derr = Math.abs(dx / dy);
+      var error = 0;
+      var x = p0.x;
+      for (var y = p0.y; (sign(dy) == 1 ? y <= p1.y : y >= p1.y); (sign(dy) == 1 ? y++ : y--)) {
+        var pout = fromGrid({x: x, y: y});
+        ctx.fillRect(pout.x, pout.y, TILESIZE, TILESIZE);
+        ctx.drawImage(Resources.icon.image, pout.x, pout.y);
+        error += derr;
+        while (error >= 0.5) {
+          x = x + sign(dx) * 1;
+          error = error - 1;
+        }
+      }
+    }    
+  }
+}
+
+// enemy behaviors
+var Simple = Object.create(Behavior);
+Simple.move = function () {
+	if (!this.resting) {
+    if (Math.abs(this.target.x - this.entity.x) > Math.abs(this.target.y - this.entity.y)) {
+      this.entity.x += sign(this.target.x - this.entity.x) * TILESIZE;
+    } else {
+      this.entity.y += sign(this.target.y - this.entity.y) * TILESIZE;
+    }
+    this.resting = true;
+  } else {
+  	this.resting = false;
+  }
+}
--- games/witch/scenes/game.js
+++ games/witch/scenes/game.js
@@ -1,58 +1,61 @@
-var onStart = function () {
-	var scene = this;
-	this.bg = this.add(Object.create(Layer).init(game.w, game.h));
-	this.bg.add(Object.create(TiledBackground).init(Resources.floor).set({x: game.w / 2, y: game.h / 2, w: game.w, h: game.h, z: 1}))
-	var witch = this.bg.add(Object.create(Sprite).init(Resources.witch).set({x: game.w / 2 - 16, y: game.h / 2 + 12, z: 2}));
+this.onStart = function () {
+	var s = this;
+	var fg = s.add(Object.create(Layer).init(game.w, game.h));
+  this.enemies = [];
+  this.summons = [];
+  var wall = fg.add(Object.create(TiledBackground).init(Resources.wall).set({x: game.w / 2, y: min.y, z: 9, w: game.w, h: 32}));
 
-	var yourturn = true;
-	var enemies = [];
-	var move = function () {
-		yourturn = false;
-		witch.add(Delay, {duration: 0.5, callback: function () {
-			for (var i = 0; i < enemies.length; i++) {
-				enemies[i].x += 32;
-			}
-			enemies.push(scene.bg.add(Object.create(Sprite).init(Resources.monster).set({x: 16, y: choose([0,1,2,3,4,5]) * 32 + 16, z: 3})));
-			yourturn = true;
-			this.entity.remove(this);
-		}});
-	}
-
-	this.onKeyDown = function (e) {
-		if (yourturn) {
-			switch(e.keyCode) {
-				case 37:
-					witch.x -= 32;
-					break;
-				case 38:
-					witch.y -= 32;
-					break;
-				case 39:
-					witch.x += 32;
-					break;
-				case 40:
-					witch.y += 32;
-					break;
-				case 32:
-					witch.animation = 1;
-					witch.frame = 0;
-					witch.frameDelay = 0;
-					break;
-			}
-			if ([37,38,39,40,32].indexOf(e.keyCode) !== -1) {
-				move();
-			}
-		}
-	}
-
+  var witch = fg.add(Object.create(Sprite).init(Resources.witch)).set({x: min.x + 2 * TILESIZE - 8, y: min.y + TILESIZE * 2, z: 10});
+  witch.spell = witch.add(Spell, {points: [], color: "darkorange", threshold: 0.1});
+  this.witch = witch;
+  witch.add(Bound, {min: {x: 8, y: min.y + 16}, max: {x: game.w - 8, y: game.h - 16}});
+  var monster = fg.add(Object.create(Sprite).init(Resources.monster)).set({x: min.x + 12 * TILESIZE - 8, y: min.y + 4 * TILESIZE, z: 9});
+  monster.enemy = monster.add(Simple, {target: witch});
+  this.enemies.push(monster);
+  fg.add(Object.create(TiledBackground).init(Resources.floor)).set({x: game.w / 2, y: min.y + 4.5 * 16 + 16, z: 8, w: game.w, h: 9 * 16});
+  this.onClick = function () {
+  	//alert('hey');
+  }
+  this.onKeyDown = function (e) {
+  	switch (e.keyCode) {
+    	case 37:
+      	witch.x -= 16;
+        break;
+      case 39:
+      	witch.x += 16;
+        break;
+      case 38:
+      	witch.y -= 16;
+        break;
+      case 40:
+      	witch.y += 16;
+        break;
+      case 81:
+      	witch.spell.set({x: witch.x, y: witch.y});
+        break;
+      case 87:
+      	witch.spell.reset();
+        break;
+    }
+    s.enemies.forEach(function (e) { e.enemy.move(); });
+  }
+	this.ready = true; // needed since not loaded from file, to add event listeners
 }
-
-var onUpdate = function (dt) {
+this.onUpdate = function (dt) {
+	var s = this;
+  if (this.enemies) {
+    for (var i = this.enemies.length - 1; i >= 0; i--) {
+      if (at(this.enemies[i], this.witch)) {
+        console.log('game over');
+        game.setScene(0, true);
+      }
+      for (var j = 0; j < this.summons.length; j++) {
+      	if (at(this.enemies[i], this.summons[j])) {
+        	this.enemies[i].alive = false;
+          this.enemies.splice(i, 1);
+          break;
+        }
+      }
+    }
+  }
 }
-
-
-var onEnd = function () {
-};
-
-var onDraw = function (ctx) {
-};
