a1034e5f78447da5959c10df11bdb82582b7c9f4
Benjamin Heller <echo29x@gmail.com>
2018-02-02 16:55:39 -0500

    passengers arrive function

--- games/mezzanine/scenes/game.js
+++ games/mezzanine/scenes/game.js
@@ -9,7 +9,7 @@ x- delayed exit destination on reaching floor
 x- health for passengers (i.e. they can tolerate 3 mistakes, X seconds waiting...)
   x- once exhausted, will start 'staircasing', i.e. going up/down one floor at a time until reaching destination (can all be done with one variable, one behavior, and a couple checkpoints?)
   - 'wait' time effect on health
-  - all health-damaging conditions (gone past, elevator changes direction [not far enough], waiting)
+  - all health-damaging conditions (x-gone past, x-elevator changes direction [not far enough], waiting)
 
 TRICKY:
 - two teams as passenger 'spawner'
@@ -59,6 +59,7 @@ this.onStart = function () {
       this.passengers[j].y = this.y;
       if (this.passengers[j].direction() !== this.direction) {
         var p = this.passengers.splice(j, 1)[0];
+        p.onelevator = false;
         p.arrive(i);
       }
     }
@@ -69,7 +70,8 @@ this.onStart = function () {
         var p = s.floors[i].passengers.splice(j, 1)[0];
         //p.follow.target = game.elevator;
         //p.follow.offset.x = -game.elevator.passengers.length * 8;
-        p.x = this.x - this.passengers.length * 8
+        p.x = this.x - this.passengers.length * 8;
+        p.onelevator = true;
         //console.log(p.follow.offset);
         this.passengers.push(p);
       }
@@ -108,17 +110,49 @@ this.onStart = function () {
       case 81:
         // create new passenger at mezzanine
         var d = randint(1, FLOORS);
-        var n = s.fg.add(Object.create(SpriteFont).init(Resources.expire_font)).set({health: 1, x: game.w / 2, y: game.h - 8, z: 5, destination: d, text: "" + d});
+        var n = s.fg.add(Object.create(SpriteFont).init(Resources.expire_font)).set({health: 3, x: game.w / 2, y: game.h - 8, z: 5, destination: d, text: "" + d});
         n.direction = function () {
           var f = tofloor(this.y);
           console.log(f, this.destination);
           return this.destination > f ? "up" : (this.destination < f ? "down" : "at");
         };
+        n.add(Behavior, {time: 0, patience: 5, update: function (dt) {
+          if (this.entity.onelevator) return;
+          else {            
+            this.time += dt;
+            if (this.time > this.patience) {
+              this.time = 0;
+              this.entity.damage();
+            }
+          }
+        }});
+        n.damage = function () {
+          this.health -= 1;
+          this.opacity = 0.25 + 0.25 * this.health;
+          var i = tofloor(this.y);
+          // staircase
+          if (this.health == 0) {
+            this.add(Periodic, {direction: sign(this.destination - i), floor: i, period: 0.5, callback: function () {
+              var n = s.floors[this.floor].passengers.indexOf(this.entity);
+              s.floors[this.floor].passengers.splice(n, 1);
+              this.floor += this.direction;
+              if (s.floors[this.floor]) {
+                s.floors[this.floor].passengers.push(this.entity);
+                this.entity.y = s.floors[this.floor].y;
+              }
+              if (this.floor === this.entity.destination) {
+                this.entity.remove(this);
+                this.entity.arrive(this.floor);
+                // fix me: trigger 'on arrive' actions -> for passenger
+              }
+            }});
+          }
+        };
         n.arrive = function (i) {
           if (this.destination === 0 && i === 0) { // remove passenger from building
             this.alive = false; // fix me: remove from FLOOR as well; make more generic check/bejavior?            
             return;
-          } 
+          }
 
           this.x = game.w / 2 + s.floors[i].passengers.length * 8;
           s.floors[i].passengers.push(this);
@@ -129,27 +163,9 @@ this.onStart = function () {
               this.entity.remove(this);
             }});
           } else { // gone past floor
-            this.health -= 1;
-            this.opacity = 0.25 + 0.25 * this.health;
-            if (this.health <= 0) {
-              // staircase
-              this.add(Periodic, {direction: sign(this.destination - i), floor: i, period: 0.5, callback: function () {
-                var n = s.floors[this.floor].passengers.indexOf(this.entity);
-                s.floors[this.floor].passengers.splice(n, 1);
-                this.floor += this.direction;
-                if (s.floors[this.floor]) {
-                  s.floors[this.floor].passengers.push(this.entity);
-                  this.entity.y = s.floors[this.floor].y;
-                }
-                if (this.floor === this.entity.destination) {
-                  this.entity.remove(this);
-                  this.entity.arrive(this.floor);
-                  // fix me: trigger 'on arrive' actions -> for passenger
-                }
-              }});
-            }
-          }          
-        }
+            this.damage();
+          }
+        };
         //n.follow = n.add(Follow, {target: s.floors[0], offset: {x: game.w / 2, y: 0, z: 1}});
         s.floors[0].passengers.push(n);
         break;
